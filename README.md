6d6b06dbe8051cf224e6213e84258bad34241ea1
Andrij Krohmalnyj <krohmalnyj.andr@gmail.com>
2024-03-19 18:06:34 +0200

    CRUD

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # FastAPI Application with Posts Data
 
-This FastAPI application serves as a simple API for accessing posts and items.
-It provides endpoints to retrieve individual posts, list all posts, and read items.
+This FastAPI application serves as a simple API for accessing posts. It provides endpoints to retrieve individual posts, list all posts, create new posts, update existing posts, and delete posts.
+
 
 # How to Run:
 
@@ -10,9 +10,9 @@ To run this FastAPI application, you need to have Python installed on your syste
 1. Install Python from python.org if you haven't already.
 
 2. Install FastAPI and Uvicorn (ASGI server) using pip:
-console : pip install fastapi uvicorn
+`pip install fastapi uvicorn`
 
 3. Run the FastAPI application using Uvicorn:
-console : uvicorn main:app --reload
+`uvicorn main:app --reload`
 
 4. Once the server starts successfully, you can access the API endpoints from your browser or API testing tools like Postman or curl.
--- main.py
+++ main.py
@@ -1,39 +1,70 @@
-from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI,status,HTTPException,Response 
+from datetime import datetime 
+from pydantic import BaseModel
 
-app = FastAPI()
+app = FastAPI() 
+ 
+class NewPost(BaseModel): 
+  title:str 
+  body:str 
+ 
+posts = [ 
+    { "id": 1, "title": "Як покращити концентрацію на робочому місці", "body": "Практичні поради та стратегії для підвищення концентрації та продуктивності під час роботи.", "data": "16.03.2024" }, 
+    { "id": 2, "title": "Ідеї для здорового сніданку", "body": "Смачні та здорові ідеї для сніданку, щоб почати день з енергією та бадьорістю.", "data": "15.06.2024" }, 
+    { "id": 3, "title": "Техніки саморозвитку, які змінять ваше життя", "body": "Дізнайтеся про ефективні техніки саморозвитку, які допоможуть вам стати кращою версією себе.", "data": "28.12.2024" }, 
+    { "id": 4, "title": "10 місць, які варто відвідати у вашому місті", "body": "Відкрийте для себе найкращі місця та атракції у вашому місті для незабутнього часу проведеного.", "data": "05.09.2024" }, 
+    { "id": 5, "title": "Секрети ефективного управління часом", "body": "Навчіться управляти своїм часом ефективно та досягати більше за короткий період.", "data": "20.11.2024" }, 
+    { "id": 6, "title": "Рецепти здорових сніданків для дітей", "body": "Смачні та поживні рецепти сніданків, які діти обожнюють і які допоможуть їм розпочати день правильно.", "data": "02.04.2024" }, 
+    { "id": 7, "title": "Як подолати стрес на роботі", "body": "Практичні поради та стратегії для ефективного управління стресом під час робочого дня.", "data": "17.10.2024" }, 
+    { "id": 8, "title": "Тренування для підвищення енергії та витривалості", "body": "Ефективні вправи та тренування для збільшення енергії та витривалості протягом дня.", "data": "08.08.2024" }, 
+    { "id": 9, "title": "5 книг для літнього читання", "body": "Список захоплюючих книг, які варто прочитати цього літа, щоб насолодитися відпочинком та розвитком.", "data": "12.01.2024" }, 
+    { "id": 10, "title": "Як знайти баланс між роботою та особистим життям", "body": "Поради та стратегії для досягнення гармонії між кар'єрою та особистим життям для збереження здоров'я та щастя.", "data": "29.05.2024" } 
+] 
+ 
+@app.get('/') 
+def read_root(): 
+  return 'Home' 
+ 
+## get 
+@app.get('/posts') 
+def read_posts(): 
+  return posts 
+ 
+@app.get('/posts/{post_id}') 
+def read_post(post_id:int): 
+  for post in posts: 
+    if post["id"] == post_id: 
+      return post 
+  raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'post with id {post_id} was not found') 
+ 
+ 
+##(post for post in posts if post["id"] == post_id)  як воно працює 
+ 
+##post 
+@app.post('/posts',status_code=status.HTTP_201_CREATED) 
+def create_post(newPost: NewPost): 
+  post = dict(newPost) 
+  post["id"] = max(post["id"] for post in posts) + 1 
+  post["date"] = datetime.now() 
+  posts.append(post) 
+  return post  
 
-posts = [
-    { "id": 1, "title": "Як покращити концентрацію на робочому місці", "body": "Практичні поради та стратегії для підвищення концентрації та продуктивності під час роботи.", "data": "16.03.2024" },
-    { "id": 2, "title": "Ідеї для здорового сніданку", "body": "Смачні та здорові ідеї для сніданку, щоб почати день з енергією та бадьорістю.", "data": "15.06.2024" },
-    { "id": 3, "title": "Техніки саморозвитку, які змінять ваше життя", "body": "Дізнайтеся про ефективні техніки саморозвитку, які допоможуть вам стати кращою версією себе.", "data": "28.12.2024" },
-    { "id": 4, "title": "10 місць, які варто відвідати у вашому місті", "body": "Відкрийте для себе найкращі місця та атракції у вашому місті для незабутнього часу проведеного.", "data": "05.09.2024" },
-    { "id": 5, "title": "Секрети ефективного управління часом", "body": "Навчіться управляти своїм часом ефективно та досягати більше за короткий період.", "data": "20.11.2024" },
-    { "id": 6, "title": "Рецепти здорових сніданків для дітей", "body": "Смачні та поживні рецепти сніданків, які діти обожнюють і які допоможуть їм розпочати день правильно.", "data": "02.04.2024" },
-    { "id": 7, "title": "Як подолати стрес на роботі", "body": "Практичні поради та стратегії для ефективного управління стресом під час робочого дня.", "data": "17.10.2024" },
-    { "id": 8, "title": "Тренування для підвищення енергії та витривалості", "body": "Ефективні вправи та тренування для збільшення енергії та витривалості протягом дня.", "data": "08.08.2024" },
-    { "id": 9, "title": "5 книг для літнього читання", "body": "Список захоплюючих книг, які варто прочитати цього літа, щоб насолодитися відпочинком та розвитком.", "data": "12.01.2024" },
-    { "id": 10, "title": "Як знайти баланс між роботою та особистим життям", "body": "Поради та стратегії для досягнення гармонії між кар'єрою та особистим життям для збереження здоров'я та щастя.", "data": "29.05.2024" }
-]
-
-
-@app.get('/')
-def read_root():
-  return '<div>Div</div>'
-
-@app.get('/items/{item_id}')
-def read_item(item_id:int , q:Union[str,None] = None):
-  return {'item_id' : item_id, 'q':q}
-
-@app.get('/posts')
-def read_posts():
-  return posts
-
-@app.get('/posts/{post_id}')
-def read_post(post_id:int):
-  return posts[post_id - 1]
-  
-
-
-
-
+##update
+@app.put('/posts/{post_id}')
+def update_posts(post_id:int,updatedPost:NewPost):
+  upPost = dict(updatedPost)
+  for post in posts: 
+    if post['id'] == post_id: 
+      post["title"] = upPost["title"]
+      post["body"] = upPost["body"]
+      return post
+  raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'post with id {post_id} was not found')
+ 
+##delete 
+@app.delete('/posts/{post_id}',status_code=status.HTTP_204_NO_CONTENT) 
+def delete_post(post_id:int): 
+  for post in posts: 
+    if post['id'] == post_id: 
+      posts.remove(post) 
+      return Response(status_code=status.HTTP_204_NO_CONTENT) 
+  raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'post with id {post_id} was not found')
