ff3420312c17bac6c00229f3fa56d528996f21cc
Andrij Krohmalnyj <krohmalnyj.andr@gmail.com>
2024-07-26 19:15:26 +0300

    upgrade code in scraper.py

--- app/scraper.py
+++ app/scraper.py
@@ -4,8 +4,6 @@ from urllib.parse import urljoin
 from abc import ABC, abstractmethod
 from schemas import NewPost
 
-
-
 class BaseScraper(ABC):
   future_posts = []
 
@@ -16,23 +14,41 @@ class BaseScraper(ABC):
   def add_post(self,post):
     self.future_posts.append(post)
 
-  @abstractmethod
-  def scrape_one_page():
-    pass
+  def scrape_one_page(self,url,next_page_link_find_params):
+    page = requests.get(url,headers=self.headers)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    self.create_posts_from_html(soup)
+    next_page_link = soup.find(**next_page_link_find_params)
+    return next_page_link
 
-  @abstractmethod
-  def scrape_all_pages():
-    pass
+  def scrape_all_pages(self,url,next_page_link_find_params):
+    next_page_link = self.scrape_one_page(url,next_page_link_find_params)
+    while next_page_link != None:
+      if next_page_link_find_params['name'] == 'a':
+        next_page_url = urljoin(url, next_page_link['href'])
+      else:
+        next_page_url = urljoin(url, next_page_link.find('a')['href'])
+      next_page_link = self.scrape_one_page(next_page_url,next_page_link_find_params)
 
   @abstractmethod
   def create_posts_from_html():
     pass
 
-  
+  @abstractmethod
+  def scrape():
+    pass
 
+
+  
 class WebScraper(BaseScraper):
   webscraper_base_url = 'https://webscraper.io/test-sites/e-commerce/static'
-  webscraper_relative_urls = ['/test-sites/e-commerce/static/computers/laptops','/test-sites/e-commerce/static/computers/tablets','/test-sites/e-commerce/static/phones/touch']
+  webscraper_relative_urls = ['/computers/laptops','/computers/tablets','/phones/touch']
+
+  next_page_link_find_params = {
+      'name':'a',
+      'class_':'page-link',
+      'attrs':{'rel':'next'}
+    }
 
   def create_posts_from_html(self,soup):
     scraped_info = soup.find_all('div',class_='caption')
@@ -43,30 +59,20 @@ class WebScraper(BaseScraper):
       post = NewPost(title=f"{title} {price}",body=description,owner_id=1)
       self.add_post(post)
 
-  def scrape_one_page(self,url):
-    page = requests.get(url,headers=self.headers)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    self.create_posts_from_html(soup)
-    next_page_link = soup.find('a',class_='page-link',attrs={'rel':'next'})
-    return next_page_link
-
-  def scrape_all_pages(self,url):
-    next_page_link = self.scrape_one_page(url)
-    while next_page_link != None:
-      next_page_url = urljoin(url, next_page_link['href'])
-      next_page_link = self.scrape_one_page(next_page_url)
-
-  def scrape_webscraper(self):
+  def scrape(self):
     for relative_url in self.webscraper_relative_urls:
-      url_to_scrap = urljoin(self.webscraper_base_url,relative_url)
-      self.scrape_all_pages(url_to_scrap)
+      url_to_scrap = self.webscraper_base_url + relative_url
+      self.scrape_all_pages(url_to_scrap,self.next_page_link_find_params)
+
  
 
-  
-
 class ToScrape(BaseScraper):
-  toscrape_base_url = 'https://books.toscrape.com/catalogue/'
-  toscrape_page1_url = toscrape_base_url + 'page-45.html'
+  toscrape_base_url = 'https://books.toscrape.com/catalogue/page-1.html'
+
+  next_page_link_find_params = {
+      'name':'li',
+      'class_':'next',
+    }
 
   def create_posts_from_html(self,soup):
     links_to_books = soup.select('.image_container a')
@@ -85,16 +91,11 @@ class ToScrape(BaseScraper):
       body = body_element.text
       post = NewPost(title=title,body=body,owner_id=1)
       return post
+    
+  def scrape(self):
+    self.scrape_all_pages(self.toscrape_base_url,self.next_page_link_find_params)
 
-  def scrape_one_page(self,url):
-    page = requests.get(url,headers=self.headers)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    self.create_posts_from_html(soup)
-    next_page_link = soup.find('li',class_='next')
-    return next_page_link
 
-  def scrape_all_pages(self):
-    next_page_link = self.scrape_one_page(self.toscrape_page1_url)
-    while next_page_link != None:
-      next_page_url = urljoin(self.toscrape_base_url, next_page_link.find('a')['href'])
-      next_page_link = self.scrape_one_page(next_page_url)
+
+
+
